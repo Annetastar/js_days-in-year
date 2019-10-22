e9d986d7c7cb877e3e15e3d1fad64cf1ff84eaa6
Anna Kazantseva <annetastara@gmail.com>
2019-10-22 16:47:01 +0300

    Fixed

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-function findYear(year) {
-  if (typeof(year) === 'number') {
-    return year % 4 === 0 && year % 100 !== 0 || year % 400 === 0;
-  }
-}
-
 function daysInYear(year) {
   try {
     if (!Number.isInteger(year) || !Number(year) > 0) {
       throw new Error();
     }
-    return findYear(year) ? 366 : 365;
+    return year % 4 === 0 && year % 100 !== 0 || year % 400 === 0
+      ? 366 : 365;
   } catch (error) {
     return 'exception';
   }
 }
+
